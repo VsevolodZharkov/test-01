5e7312ad8d944b6613a7370675d520c079356eb9
vsevolodjarkov <94003560+vsevolodjarkov@users.noreply.github.com>
2022-09-23 13:07:31 +0300

    save

--- src/index.js
+++ src/index.js
@@ -2,22 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { persistor, store } from './redux/store';
-// import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <PersistGate loading={null} persistor={persistor}>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </PersistGate>
-//   </Provider>
-// );
